de687bf0094741dac77fac471d28bc1cf34379d7
Ali Shah <ashah@vtsl.net>
2019-01-06 15:44:02 +0000

    Created a page

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <v-toolbar color="primary" dark fixed app>
+      <!-- <v-toolbar-side-icon v-on:click.stop="drawer = !drawer"></v-toolbar-side-icon> -->
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Misbah Shah</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+           <h1>{{title}}</h1>
+        <h2>{{subHeading}}</h2>
+        <v-img :src="productImage"></v-img>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex text-xs-center>
+            Here is the Centre
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="primary" app>
+      <span class="white--text">&copy; {{currentYear}}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+  props: {
+    source: String,
+    msg: String
+  },
+  data: () => ({
+    drawer: null,
+    currentYear: null,
+    title: null,
+    subHeading: null,
+    productImg: "https://misbahshah.com/franquias/2/371212/editor-html/2402853.png",
+    
+  }),
+  methods: {
+    getCurrentYear() {
+      this.currentYear = new Date().getFullYear()
+    }
+  },
+  created() {
+     this.getCurrentYear()
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.brown.darken2, // #E53935
+    secondary: colors.brown.darken2, // #FFCDD2
+    accent: colors.brown.darken2 // #3F51B5
+  }
+})
 
 new Vue({
   render: h => h(App),
