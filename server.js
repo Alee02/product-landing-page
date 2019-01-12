a44106b54ab330cdf9d87204138300af80cdc0b9
Ali Shah <ashah@vtsl.net>
2019-01-12 12:52:53 +0000

    Added server

--- server.js
+++ server.js
@@ -9,6 +9,4 @@ app.use(serveStatic(path.join(__dirname, 'dist')));
 //     res.sendFile(__dirname, "/dist/index.html")
 // })
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log("process running on port 8080");
-});
+app.listen(process.env.PORT || 8080)
