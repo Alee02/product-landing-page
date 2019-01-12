1f63177a5dbebb47af4e3225099f68a34ceaaf36
Ali Shah <ashah@vtsl.net>
2019-01-12 12:28:32 +0000

    Another test

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 // app.use(express.static(__dirname, 'dist/'))
-app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname, "/dist/index.html")
-})
+// app.get(/.*/, function(req, res) {
+//     res.sendFile(__dirname, "/dist/index.html")
+// })
 
-app.listen(process.env.PORT || 8080, function() {
+app.listen(process.env.PORT || 8080, () => {
     console.log("process running on port 8080");
 });
