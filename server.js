e7c3bf986b09eae3199a6fac09cbc4cee03a7297
Ali Shah <ashah@vtsl.net>
2019-01-12 12:07:53 +0000

    Changed port

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname, '/dist/'))
@@ -9,4 +8,4 @@ app.get(/.*/, function(req, res) {
 // const path = require('path');
 // const serveStatic = require("serve-static")
 // app.use(serveStatic(path.join(__dirname, 'dist')));
-app.listen(port);
+app.listen(process.env.PORT || 8080);
