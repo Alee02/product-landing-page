81175990b01c75564ca60189c98ee9bf42e2e581
Ali Shah <ashah@vtsl.net>
2019-01-14 23:20:31 +0000

    Added another

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -3,15 +3,17 @@
       <h1>Reviews</h1>
       <v-flex>
       <h4>Is Jahan mein koi ahle dil baat jab keh na sake, To woh Misbah Shah ki shairy ka sahar le</h4>
-      <p>By Ali Irfan Shah (Novelist, Phd Psychologist and Masters in Urdu Language)</p>
+      <p>By Ali Irfan Shah (Novelist, Phd Psychologist and Masters in Urdu Language) (Probably should translate)</p>
       </v-flex>
       <v-flex>
-        <h4>Could write anything</h4>
+        <h4>Could write anything. Parveen shakir of our times</h4>
         <p>Dr P. Zafar (M.B.B.S RMP Gynacologist)</p>
       </v-flex>
       <v-flex>
-        <h4></h4>
-        <p></p>
+        <h4>Misbah shah ki shairy bohot kuch sochne par majboor karti Hai. Har sher mukamal iqai kahani hai
+          Asi kahani jo suni nahi leikin ankhon se dekh li Hai.
+        </h4>
+        <p>Prof A. K. Hayat</p>
       </v-flex>
     
   </div>
