c004fbbfa3141395a5336f275afdc1c46398f5dc
Ali Shah <alishah@Alis-MacBook-Air-2.local>
2019-01-25 18:33:27 +0000

    Change

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require("express")
-const path = require('path');
-const serveStatic = require("serve-static")
-const app = express();
-
-app.use(serveStatic(path.join(__dirname, 'dist')));
-// app.use(express.static(__dirname, 'dist/'))
-// app.get(/.*/, function(req, res) {
-//     res.sendFile(__dirname, "/dist/index.html")
-// })
-
-app.listen(process.env.PORT || 80)
+//Server.js, don't forget to add express & ejs to packages
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3003
+const router = express.Router()
+app.use(express.static(`${__dirname}/dist`)) // set the static files location for the static html
+app.engine('.html', require('ejs').renderFile)
+app.set('views', `${__dirname}/dist`)
+router.get('/*', (req, res, next) => {
+  res.sendFile(`${__dirname}/dist/index.html`)
+})
+app.use('/', router)
+app.listen(port)
+console.log('App running on port', port)
